882cecdcaab350071d0a890a311ddd5bd465d1b7
Tomasz Bazan <tomasz.bazan.89@gmail.com>
2025-03-20 22:10:46 +0100

    Added service to run a main functionality in the background (#1)

--- lib/converters/duration_to_string.dart
+++ lib/converters/duration_to_string.dart
@@ -2,17 +2,17 @@ class DurationToString {
   static String twoDigits(int n) => n.toString().padLeft(2, "0");
 
   static String convert(Duration value) {
-    String negativeSign = value.isNegative ? '-' : '';
+    String negativeSign = value.isNegative ? '- ' : '';
     String twoDigitHours = twoDigits(value.inHours.remainder(60));
     String twoDigitMinutes = twoDigits(value.inMinutes.remainder(60).abs());
     String twoDigitSeconds = twoDigits(value.inSeconds.remainder(60).abs());
-    return "$negativeSign $twoDigitHours:$twoDigitMinutes:$twoDigitSeconds";
+    return "$negativeSign$twoDigitHours:$twoDigitMinutes:$twoDigitSeconds";
   }
 
   static String shortConvert(Duration value) {
-    String negativeSign = value.isNegative ? '-' : '';
+    String negativeSign = value.isNegative ? '- ' : '';
     String twoDigitHours = twoDigits(value.inHours.remainder(60).abs());
     String twoDigitMinutes = twoDigits(value.inMinutes.remainder(60).abs());
-    return "$negativeSign $twoDigitHours:$twoDigitMinutes";
+    return "$negativeSign$twoDigitHours:$twoDigitMinutes";
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
-import 'package:atloud/clock/clock.dart';
+import 'package:atloud/foreground_task/foreground_task_initializer.dart';
+import 'package:atloud/settings/settings.dart';
+import 'package:atloud/shared/available_page.dart';
 import 'package:atloud/theme/theme.dart';
+import 'package:atloud/timer/timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterForegroundTask.initCommunicationPort();
+  ForegroundTaskInitializer.initService();
+
+  // var lastVisitedPage = await UserDataStorage.lastVisitedPageValue(); // TODO: Verify if it is working in all cases
+  var lastVisitedPage = AvailablePage.clock;
+
+  runApp(MyApp(lastVisitedPage: lastVisitedPage));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AvailablePage lastVisitedPage;
+  const MyApp({super.key, required this.lastVisitedPage});
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +28,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Atloud',
       theme: CustomTheme.lightTheme,
-      home: const ClockPage(),
+      home: TimerPage(isTimerPage: lastVisitedPage == AvailablePage.timer ? true : false),
+      routes: {
+        '/timer': (context) => const TimerPage(isTimerPage: true),
+        '/clock': (context) => const TimerPage(isTimerPage: false),
+        '/settings': (context) => const SettingsPage(),
+      },
     );
   }
 }
-
