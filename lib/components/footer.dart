999ae2152bf0a99b6ba6e52992869d1e899e793b
Your Name <you@example.com>
2025-03-21 09:58:53 +0100

    Adjust colors in navigation bar

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -1,11 +1,11 @@
 import 'package:atloud/settings/settings.dart';
 import 'package:atloud/theme/colors.dart';
+import 'package:atloud/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 import '../theme/fonts.dart';
 
 class FooterWidget extends StatelessWidget {
-  final _buttonSize = 70.0;
   final String text;
   final Function() actionOnText;
   final Function()? cleanAction;
@@ -15,33 +15,40 @@ class FooterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 25.0),
-      child: Row(
-        children: [
-          SizedBox(width: _buttonSize),
-          Expanded(
-            child: TextButton(
+      color: CustomColors.footerBackgroundColor,
+      child: SafeArea(
+        child: Stack(
+          children: [
+            Center(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                ),
                 onPressed: actionOnText,
                 child: Text(
                   text,
                   textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: CustomColors.textColor,
-                    fontFamily: CustomFonts.openSans.value,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 4.0,
-                  ),
-                )),
-          ),
-          IconButton(
-            icon: Icon(Icons.settings, size: _buttonSize, color: CustomColors.textColor),
-            onPressed: () {
-              cleanAction?.call();
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingsPage()));
-            },
-          ),
-        ],
+                  style: CustomTheme.bottomButtonTheme,
+                ),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.settings,
+                  size: 32,
+                  color: CustomColors.footerTextColor,
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 24.0),
+                onPressed: () {
+                  cleanAction?.call();
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const SettingsPage()));
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
