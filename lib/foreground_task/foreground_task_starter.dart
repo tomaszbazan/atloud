c9c4d8e5e9a5af68e1d22d72b6458cb8fb86ebed
Your Name <you@example.com>
2025-08-19 22:22:19 +0200

    Add multilingual support for duration notifications and speech

--- lib/foreground_task/foreground_task_starter.dart
+++ lib/foreground_task/foreground_task_starter.dart
@@ -2,6 +2,9 @@ import 'package:atloud/foreground_task/timer_task.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
+import '../l10n/supported_language.dart';
+import '../shared/user_data_storage.dart';
+
 @pragma('vm:entry-point')
 void startCallback() {
   FlutterForegroundTask.setTaskHandler(TimerTaskHandler());
@@ -10,18 +13,47 @@ void startCallback() {
 class ForegroundTaskStarter {
   static void startService(receivedServiceData) async {
     // Without this block service is not starting
-    if (await FlutterForegroundTask.isRunningService) {
-    }
+    if (await FlutterForegroundTask.isRunningService) {}
     FlutterForegroundTask.addTaskDataCallback(receivedServiceData);
+    var languageCode = await UserDataStorage.languageValue();
+    var language = SupportedLanguage.fromCode(languageCode);
+
     FlutterForegroundTask.startService(
-      serviceId: 8882,
-      notificationTitle: 'At Loud!',
-      notificationText: 'Naciśnij aby powrócić do aplikacji',
-      notificationIcon: const NotificationIcon(metaDataName: 'pl.btsoftware.atloud.default_notification_icon', backgroundColor: Colors.black),
-      notificationInitialRoute: '/clock',
-      callback: startCallback,
-      notificationButtons: [const NotificationButton(id: TimerTaskHandler.stopButtonId, text: 'Zatrzymaj')]
+        serviceId: 8882,
+        notificationTitle: 'At Loud!',
+        notificationText: notificationText(language),
+        notificationIcon: const NotificationIcon(
+            metaDataName: 'pl.btsoftware.atloud.default_notification_icon',
+            backgroundColor: Colors.black),
+        notificationInitialRoute: '/clock',
+        callback: startCallback,
+        notificationButtons: [
+          NotificationButton(
+              id: TimerTaskHandler.stopButtonId, text: stopButtonText(language))
+        ]
     );
+  }
+
+  static String notificationText(language) {
+    switch (language) {
+      case SupportedLanguage.polish:
+        return 'Naciśnij aby powrócić do aplikacji';
+      case SupportedLanguage.english:
+        return 'Press to return to the application';
+      default:
+        return 'Press to return to the application';
+    }
+  }
+
+  static String stopButtonText(language) {
+    switch (language) {
+      case SupportedLanguage.polish:
+        return 'Zatrzymaj';
+      case SupportedLanguage.english:
+        return 'Stop';
+      default:
+        return 'Stop';
+    }
   }
 
   static Future<ServiceRequestResult> stopService() {
